cafea5379c2aa992fa29b5d89c2661121ab70515
grozeageorge <grozea.george03@gmail.com>
2025-11-03 14:09:19 +0200

    first commit

---
+++ src/test/java/com/example/yt_tv/YtTvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yt_tv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtTvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
