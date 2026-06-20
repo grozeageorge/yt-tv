75641dc55fd40bc40006539aff503c75c9da40fa
grozeageorge <grozea.george03@gmail.com>
2026-06-20 15:32:28 +0300

    feat: update CI/CD pipeline to build services and dependencies instead of common module

--- content-service/src/main/java/com/example/yt_tv/ContentApplication.java
+++ content-service/src/main/java/com/example/yt_tv/ContentApplication.java
@@ -13,6 +13,6 @@ public class ContentApplication {
         SpringApplication.run(ContentApplication.class, args);
 
         // Added comment to test aws ci/cd with github actions.
-        System.out.println("CI/CD Pipeline Test V4");
+        System.out.println("CI/CD Pipeline Test V5");
     }
 }
